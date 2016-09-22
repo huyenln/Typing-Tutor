8465ca7d03f769be5d9b5daff1de452906636e3f
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2016-09-23 00:15:35 +0700

    Add a test paragraph page

--- js/typing.js
+++ js/typing.js
@@ -1,4 +1,5 @@
 $(function() {
+    $('#ready-popup').modal('show');
     var text = $('#typing-field').text();
     var current = 0;
     var charsTyped = 0.0;
@@ -14,7 +15,6 @@ $(function() {
             $('#' + (current+1)).addClass('text-current');
             current++;
             if (current == text.length) {
-                $('#index-modal').modal('hide');
                 alert("Congratulations! You finished this exercise!");
             }
             else {
@@ -28,17 +28,7 @@ $(function() {
         $('#cpm').text(Math.round(charsTyped / ((timer-start)/1000) * 60));
         $('#accuracy').text(Math.round(current / charsTyped * 100));
     };
-// Reset everything when clicking the "Start Typing" button
-    $('#modalToggle').on('click', function(e) {
-        $('#' + current).removeClass('text-current');
-        $('#' + current).removeClass('text-error'); 
-        $('#0').addClass('text-current');
-        current = 0;
-        charsTyped = 0.0;
-        start = 0;
-        timer = 0;
-        cpm = 0;
-    });
+
 // timer
     function pad ( val ) { return val > 9 ? val : "0" + val; }
     function setTime () {
@@ -49,25 +39,23 @@ $(function() {
     }
 
 // Starts timer when the dialog is shown
-    $("#index-modal").on("shown.bs.modal", function(e) {
+   $('#ready-popup').on("hidden.bs.modal", function(e) {
         start = (new Date).getTime();
         setInterval(setTime, 1000);
-    });
-
-
-// Key event handler
-    window.onkeydown = function(event) {
-        if (event.keyCode == 32) {
-            event.preventDefault();
-            checkChar(" ");
+        // Key event handler
+        window.onkeydown = function(event) {
+            if (event.keyCode == 32) {
+                event.preventDefault();
+                checkChar(" ");
+            }
+            if (event.keyCode == 9) {
+                // Tab
+                event.preventDefault();
+            }
         }
-        if (event.keyCode == 9) {
-            // Tab
-            event.preventDefault();
-        }
-    }
-    $(document).keypress(function(event) {
-        checkChar(String.fromCharCode(event.which));
+        $(document).keypress(function(event) {
+            checkChar(String.fromCharCode(event.which));
+        });
     });
 
 // Focus trick by Giang
