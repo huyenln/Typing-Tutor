032e48987f3b5d95b1fce123233d2a17f767189d
Phạm Nguyễn Hoàng <pnhoang96@gmail.com>
2016-09-22 21:52:59 +0700

    Remove unnecessary files

--- js.js
+++
@@ -1,9 +0,0 @@
-window.onkeydown = function(e) { 
-  return !(e.keyCode == 32 && e.target == document.body);
-}; 
-$(document).ready(function(){
-	$("#myBtn").click(function(){
-		$("#myText").css("color" , "red");
-		alert($('#myInput').val());
-	})
-})
